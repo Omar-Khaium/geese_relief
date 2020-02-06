84b344c516fb019c79d088b979619408932e2d15
Omar Khaium Chowdhury <aunto.aiub@gmail.com>
2020-02-06 12:09:01 +0600

    PDF Hided

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_grate_app/ui/ui_launcher.dart';
-import 'package:flutter_grate_app/widgets/PDFScreen.dart';
 
 void main() {
   runApp(
